968ac74ea65722abe379a64b86136500f44f21e0
Rodolfo Carvalho <rhcarvalho@gmail.com>
2013-01-31 05:32:51 +0800

    First simple implementation of a DAWG.
    The minimal functionality have been implemented, accompanied by tests.
    More to come.

---
+++ dawg/dawg.go
@@ -0,0 +1,96 @@
+// Package dawg provides a Directed Acyclic Word Graph.
+// A DAWG is a data structure optimized for fast string lookups.
+package dawg
+
+// A DAWG is the main type provided by this package.
+type DAWG struct {
+	root *node
+}
+
+// A node is a recursive structure that represents a node of a DAWG.
+type node struct {
+	children map[nodeValue]*node
+	eow      bool
+}
+
+// A nodeValue is the type stored in the value field of each node.
+type nodeValue rune
+
+// New creates a new DAWG from a vocabulary.
+func New(vocabulary []string) *DAWG {
+	d := &DAWG{&node{}}
+	for _, word := range vocabulary {
+		d.Insert(word)
+	}
+	return d
+}
+
+// Insert a word into the DAWG.
+func (d *DAWG) Insert(word string) {
+	current := d.root
+	for _, k := range []nodeValue(word) {
+		if current.children == nil {
+			current.children = make(map[nodeValue]*node)
+		}
+		if next, ok := current.children[k]; ok {
+			current = next
+		} else {
+			next = &node{}
+			current.children[k] = next
+			current = next
+		}
+	}
+	current.eow = true
+}
+
+// Contains return true when the word is in the DAWG.
+func (d *DAWG) Contains(word string) bool {
+	current := d.root
+	for _, k := range []nodeValue(word) {
+		if current.children == nil {
+			return false
+		}
+		if next, ok := current.children[k]; ok {
+			current = next
+		} else {
+			return false
+		}
+	}
+	return current.eow
+}
+
+// Returns a list of words of this DAWG that are prefixes of the given word.
+func (d *DAWG) Prefixes(word string) []string {
+	current := d.root
+	res := []string{}
+	for i, k := range []nodeValue(word) {
+		if current.children == nil {
+			break
+		}
+		if next, ok := current.children[k]; ok {
+			current = next
+			if current.eow {
+				res = append(res, word[:i+1])
+			}
+		} else {
+			break
+		}
+	}
+	return res
+}
+
+// Returns a channel filled with words of this DAWG that are prefixes of the given word.
+// Not implemented
+func (d *DAWG) IterPrefixes(word string) chan string {
+	return make(chan string)
+}
+
+// BUG(rhcarvalho): IterPrefixes is not implemented yet.
+
+// Compact the DAWG by sharing common suffixes.
+// Returns the number of trimmed branches.
+func (d *DAWG) Compact() int {
+	return 0
+}
+
+// BUG(rhcarvalho): Compact is not implemented yet.
---
+++ dawg/dawg_test.go
@@ -0,0 +1,221 @@
+package dawg
+
+import (
+	"bytes"
+	"fmt"
+	"strings"
+	"testing"
+)
+
+// the map keys are space-separated words to construct the DAWG.
+var dawgs = map[string]*DAWG{
+	"": &DAWG{&node{}},
+	"g": &DAWG{&node{children: map[nodeValue]*node{
+		'g': &node{eow: true},
+	}}},
+	"go": &DAWG{&node{children: map[nodeValue]*node{
+		'g': &node{children: map[nodeValue]*node{
+			'o': &node{eow: true},
+		}},
+	}}},
+	"g go": &DAWG{&node{children: map[nodeValue]*node{
+		'g': &node{eow: true, children: map[nodeValue]*node{
+			'o': &node{eow: true},
+		}},
+	}}},
+	"g t": &DAWG{&node{children: map[nodeValue]*node{
+		'g': &node{eow: true},
+		't': &node{eow: true},
+	}}},
+	"go t": &DAWG{&node{children: map[nodeValue]*node{
+		'g': &node{children: map[nodeValue]*node{
+			'o': &node{eow: true},
+		}},
+		't': &node{eow: true},
+	}}},
+}
+
+func TestNew(t *testing.T) {
+	for words, d := range dawgs {
+		nd := New(strings.Fields(words))
+		if !dawgsEqual(nd, d) {
+			t.Errorf("DAWG should be %v, got %v", d, nd)
+		}
+	}
+}
+
+func TestContains(t *testing.T) {
+	type query struct {
+		key string
+		res bool
+	}
+	tests := []struct {
+		words   string
+		queries []query
+	}{{
+		"g", []query{
+			{"g", true},
+			{"go", false},
+			{"z", false},
+		}}, {
+		"go", []query{
+			{"g", false},
+			{"go", true},
+			{"golang", false},
+		}}, {
+		"g go", []query{
+			{"g", true},
+			{"go", true},
+			{"golang", false},
+		}}, {
+		"g t", []query{
+			{"g", true},
+			{"t", true},
+			{"golang", false},
+			{"tornado", false},
+			{"z", false},
+		}}, {
+		"go t", []query{
+			{"g", false},
+			{"go", true},
+			{"t", true},
+			{"golang", false},
+			{"tornado", false},
+			{"z", false},
+		}},
+	}
+	for _, test := range tests {
+		d, ok := dawgs[test.words]
+		if !ok {
+			t.Errorf("Missing DAWG for words %#v", test.words)
+			continue
+		}
+		for _, q := range test.queries {
+			if ok := d.Contains(q.key); ok != q.res {
+				t.Errorf("DAWG(%#v).Contains(%#v) should be %v, got %v", test.words, q.key, q.res, ok)
+			}
+		}
+	}
+}
+
+func TestPrefixes(t *testing.T) {
+	type query struct {
+		key string
+		res []string
+	}
+	tests := []struct {
+		words   string
+		queries []query
+	}{{
+		"g go", []query{
+			{"", []string{}},
+			{"g", []string{"g"}},
+			{"go", []string{"g", "go"}},
+			{"golang", []string{"g", "go"}},
+			{"python", []string{}},
+		}}, {
+		"g t", []query{
+			{"g", []string{"g"}},
+			{"t", []string{"t"}},
+			{"golang", []string{"g"}},
+			{"tornado", []string{"t"}},
+			{"z", []string{}},
+		}}, {
+		"", []query{
+			{"", []string{}},
+			{"g", []string{}},
+			{"golang", []string{}},
+		}},
+	}
+	for _, test := range tests {
+		d, ok := dawgs[test.words]
+		if !ok {
+			t.Errorf("Missing DAWG for words %#v", test.words)
+			continue
+		}
+		for _, q := range test.queries {
+			if prefs := d.Prefixes(q.key); !slicesEqual(prefs, q.res) {
+				t.Errorf("DAWG(%#v).Prefixes(%#v) should be %v, got %v", test.words, q.key, q.res, prefs)
+			}
+		}
+	}
+}
+
+// Helper functions for printings DAWGs.
+func (d *DAWG) String() string {
+	return fmt.Sprintf("&DAWG{%v}", d.root)
+}
+
+func (n *node) String() string {
+	eowS, chldS := eowToString(n.eow), childrenToString(n.children)
+	var sep string
+	if eowS != "" && chldS != "" {
+		sep = ", "
+	} else {
+		sep = ""
+	}
+	return fmt.Sprintf("&node{%s%s%s}", eowS, sep, chldS)
+}
+
+func eowToString(eow bool) string {
+	if eow {
+		return "eow: true"
+	}
+	return ""
+}
+
+func childrenToString(children map[nodeValue]*node) string {
+	if children == nil {
+		return ""
+	}
+	b := bytes.NewBufferString("children: map[nodeValue]*node{\n")
+	for k, nd := range children {
+		fmt.Fprintf(b, "'%s': %v,\n", k, nd)
+	}
+	b.WriteByte('}')
+	return b.String()
+}
+
+func (nv nodeValue) String() string {
+	return string(nv)
+}
+
+// Helper functions for comparing DAWGs.
+func dawgsEqual(d1, d2 *DAWG) bool {
+	return nodesEqual(d1.root, d2.root)
+}
+
+func nodesEqual(x, y *node) bool {
+	if x == nil {
+		return y == nil
+	}
+	if y == nil {
+		return false
+	}
+	if x.eow != y.eow {
+		return false
+	}
+	for key, xChildNode := range x.children {
+		if yChildNode, ok := y.children[key]; !ok {
+			return false
+		} else {
+			if !nodesEqual(xChildNode, yChildNode) {
+				return false
+			}
+		}
+	}
+	return true
+}
+
+// sliceEq defines equality based on its contents.
+func slicesEqual(a, b []string) bool {
+	if len(a) != len(b) {
+		return false
+	}
+	for i := range a {
+		if a[i] != b[i] {
+			return false
+		}
+	}
+	return true
+}
